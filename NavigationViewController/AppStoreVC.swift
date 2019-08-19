259ad3cf69038a15be23edda4c086e3bca8177fc
mohamedosama2074 <iosengks74@gmail.com>
2019-08-19 13:29:57 +0200

    UINavigationController

--- NavigationViewController/AppStoreVC.swift
+++ NavigationViewController/AppStoreVC.swift
@@ -1,21 +1,22 @@
 //
-//  ViewController.swift
+//  AppStoreVC.swift
 //  NavigationViewController
 //
 //  Created by Mohamed on 8/19/19.
 //  Copyright © 2019 Mohamed74. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
-
+class AppStore : UIViewController {
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         
-        
+        title = "App Store"
     }
-
-
+    
+    
 }
-
